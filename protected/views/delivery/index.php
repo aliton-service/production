a91e8fd2b2b03e4c9221de1c68053e2aa2ba18a0
rpy <rpy@IT-PC-01.Aliton.net>
2016-08-18 15:26:07 +0300

    Merge origin/production into production
    Conflicts:
    	js/aliton-sources.js

--- protected/views/delivery/index.php
+++ protected/views/delivery/index.php
@@ -38,11 +38,12 @@
         $('#btnUndoExec').jqxButton($.extend(true, {}, ButtonDefaultSettings, { disabled: true, width: 160, height: 30 }));
         $('#btnRefresh').jqxButton($.extend(true, {}, ButtonDefaultSettings, { width: 120, height: 30 }));
         $('#btnPrint').on('click', function(){
-            window.open(<?php echo json_encode(Yii::app()->createUrl('Reports/ReportOpen', array(
-                'ReportName' => '/Заявки на доставку/Заявка на доставку',
-                'Ajax' => false,
-                'Render' => true,
-            ))); ?> + '&Parameters[Dldm_id]=' + DeliveryDemand.dldm_id);
+            window.open('http://aliton/index.php?r=Reports/ReportOpen&ReportName=%2F%D0%97%D0%B0%D1%8F%D0%B2%D0%BA%D0%B8+%D0%BD%D0%B0+%D0%B4%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D1%83%2F%D0%97%D0%B0%D1%8F%D0%B2%D0%BA%D0%B0+%D0%BD%D0%B0+%D0%B4%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D1%83&Ajax=&Render=1&Parameters[Dldm_id]=59885');
+        //            window.open(<?php // echo json_encode(Yii::app()->createUrl('Reports/ReportOpen', array(
+//                'ReportName' => '/Заявки на доставку/Заявка на доставку',
+//                'Ajax' => false,
+//                'Render' => true,
+//            ))); ?>// + '&Parameters[Dldm_id]=' + DeliveryDemand.dldm_id);
         });
         $('#btnAccept').on('click', function(){
             $.ajax({
