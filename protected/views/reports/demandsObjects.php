1aaa9bae306f21c7036555d50fca5f17044e3f5b
fiy <fiy@IT-PC-03.Aliton.net>
2016-09-20 18:24:26 +0300

    Merge origin/production into production
    Conflicts:
    	js/aliton-sources.js
    	protected/views/monitoringDemands/index.php

--- protected/views/reports/demandsObjects.php
+++ protected/views/reports/demandsObjects.php
@@ -12,6 +12,11 @@
             
             var DataServiceType = new $.jqx.dataAdapter(Sources.SourceServiceTypes);
             var DataTerritory = new $.jqx.dataAdapter(Sources.SourceTerritory);
+            var DataExec = [
+                {id: 1, name: 'Все'},
+                {id: 2, name: 'Непереданные'},
+                {id: 3, name: 'Невыполненные'},
+            ];
             
             $("#Address").jqxComboBox($.extend(true, {}, ComboBoxDefaultSettings, { source: DataAddresses, displayMember: "Addr", valueMember: "Address_id", width: 400 }));
             $("#Master").jqxComboBox($.extend(true, {}, ComboBoxDefaultSettings, { source: DataEmpl, displayMember: "ShortName", valueMember: "Employee_id", width: 200 }));
@@ -19,6 +24,7 @@
             $("#SystemType").jqxComboBox($.extend(true, {}, ComboBoxDefaultSettings, { source: DataSystemType, displayMember: "SystemTypeName", valueMember: "SystemType_Id", width: 280 }));
             $("#EquipType").jqxComboBox($.extend(true, {}, ComboBoxDefaultSettings, { source: DataEquipType, displayMember: "EquipType", valueMember: "EquipType_id", width: 280 }));
             $("#Malfunction").jqxComboBox($.extend(true, {}, ComboBoxDefaultSettings, { source: DataMalfunction, displayMember: "Malfunction", valueMember: "Malfunction_id", width: 280 }));
+            $("#Exec").jqxComboBox($.extend(true, {}, ComboBoxDefaultSettings, { source: DataExec, displayMember: "name", valueMember: "id", width: 280 }));
             
             $("#ServiceType").jqxComboBox($.extend(true, {}, ComboBoxDefaultSettings, { source: DataServiceType, displayMember: "ServiceType", valueMember: "ServiceType_id", width: 280 }));
             $("#Territory").jqxComboBox($.extend(true, {}, ComboBoxDefaultSettings, { source: DataTerritory, displayMember: "Territ_Name", valueMember: "Territ_Id", width: 200 }));
@@ -28,6 +34,10 @@
             
             $("#DateExecStart").jqxDateTimeInput($.extend(true, {}, DateTimeDefaultSettings, { width: 110, formatString: 'dd.MM.yyyy', value: null }));
             $("#DateExecEnd").jqxDateTimeInput($.extend(true, {}, DateTimeDefaultSettings, { width: 110, formatString: 'dd.MM.yyyy' }));
+            
+            
+            $("#AllPeriod").jqxCheckBox($.extend(true, {}, CheckBoxDefaultSettings));
+            $("#AllExecPeriod").jqxCheckBox($.extend(true, {}, CheckBoxDefaultSettings));
             
              
             var NewDate = new Date();
@@ -90,17 +100,18 @@
         <div class="row-column"><div id='ServiceType' name="Parameters[prmServiceType]"></div></div>
     </div>
     
-<!--    <div class="row">
+    <div class="row">
         <div class="row-column">Выполнение: </div>
-        <div class="row-column"><div id='' name="Parameters[]"></div></div>
-    </div>-->
+        <div class="row-column"><div id='Exec' name="Parameters[prmExec]"></div></div>
+    </div>
 
     <div class="row">
         <div class="row-column">Участок: </div>
         <div class="row-column"><div id='Territory' name="Parameters[prmTerrit1]"></div></div>
     </div>
 
-    <div class="row" style="padding: 10px; width: 300px; border: 1px solid #ddd; background-color: #F2F2F2;">
+    <div class="row" style="padding: 10px; width: 700px; border: 1px solid #ddd; background-color: #F2F2F2;">
+        <div class="row-column" id="AllPeriod" name="Parameters[prmAllPeriod]"></div>
         <div class="row" style="margin: 0 0 15px 0; width: 100%;">Дата регистрации заявки за период:</div>
         <div class="row">
             <div class="row-column" style="margin-top: 2px;">с </div><div class="row-column"><div id='DateStart' name="Parameters[prmDateStart]"></div></div>
@@ -109,6 +120,7 @@
     </div>
 
     <div class="row" style="padding: 10px; width: 300px; border: 1px solid #ddd; background-color: #F2F2F2;">
+        <div class="row-column" id="AllExecPeriod" name="Parameters[prmAllExecPeriod]"></div>
         <div class="row" style="margin: 0 0 15px 0; width: 100%;">Дата выполнения заявки за период:</div>
         <div class="row">
             <div class="row-column" style="margin-top: 2px;">с </div><div class="row-column"><div id='DateExecStart' name="Parameters[prmDateStart]"></div></div>
