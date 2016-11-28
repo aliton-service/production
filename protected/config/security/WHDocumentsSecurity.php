781676f1f339ab3697d2ae0bc6d337c17e1c32df
rpy <rpy@IT-PC-01.Aliton.net>
2016-11-28 12:18:18 +0300

    Merge origin/production into production
    Conflicts:
    	js/aliton-sources.js
    	protected/config/auth.php

--- protected/config/security/WHDocumentsSecurity.php
+++ protected/config/security/WHDocumentsSecurity.php
@@ -47,6 +47,7 @@
                 'WHDocumentsView',
                 'CreateWHDocuments',
                 'AddNoteWHDocuments',
+                'UpdateWHDocuments',
             ),
         ),
         
