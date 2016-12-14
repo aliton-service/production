23901c03d6b11c0271f6f7554eccc41e55e2eb67
rpy <rpy@IT-PC-01.Aliton.net>
2016-12-14 11:14:39 +0300

    Merge origin/production into production
    Conflicts:
    	js/aliton-sources.js
    	protected/config/auth.php

--- protected/models/ObjectsGroupSystems.php
+++ protected/models/ObjectsGroupSystems.php
@@ -24,7 +24,7 @@ class ObjectsGroupSystems extends MainFormModel
     public function rules() {
         return array(
             /* обязательные поля*/
-            array('Sttp_id, ObjectGr_id, Availability_id, count, Condition', 'required'),
+            array('Sttp_id, ObjectGr_id, Availability_id, count', 'required'),
             array('ObjectsGroupSystem_id,'
                 . 'SystemTypeName,'
                 . 'CustomerName,'
