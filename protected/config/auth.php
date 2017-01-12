a216776028313b3a48ddb3dc552a7cf4db97354a
rpy <rpy@IT-PC-01.Aliton.net>
2017-01-12 17:46:55 +0300

    Merge origin/production into production
    Conflicts:
    	js/aliton-sources.js
    	protected/config/auth.php

--- protected/config/auth.php
+++ protected/config/auth.php
@@ -1125,6 +1125,7 @@ return array_merge(
             'children' => array(
                 'StaffManager',
                 'ManagerObjectEquips',
+                'AdminContractEquips',
             ),
         ),
         
