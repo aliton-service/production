78edd95ad607a37d1b038b467471bb6508fa228e
fiy <fiy@IT-PC-03.Aliton.net>
2017-06-13 17:08:21 +0300

    Merge origin/production into production
    Conflicts:
    	protected/views/docmachsdetails/_form.php
    	protected/views/whdocuments/view.php

--- protected/config/auth.php
+++ protected/config/auth.php
@@ -1419,6 +1419,7 @@ return array_merge(
                 'AdminWHDocuments',
                 'AdminSuppliers',
                 'ManagerWhActs',
+                'AdminEqipGroups',
             ),
         ),
         
