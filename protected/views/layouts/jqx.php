7e0670a7185742d036a99df7b73489d720c5a439
rpy <rpy@IT-PC-01.Aliton.net>
2017-06-22 12:12:40 +0300

    Merge origin/production into production
    Conflicts:
    	js/aliton-sources.js
    	protected/config/auth.php

--- protected/views/layouts/jqx.php
+++ protected/views/layouts/jqx.php
@@ -5,8 +5,6 @@
     <!-- Подключаем таблицу стилей -->
 
     <link rel="stylesheet" href="/js/jqwidgets/styles/jqx.base.css" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/header.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-1.11.3.min.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jqwidgets/localization.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jqwidgets/jqx-all.js"></script>
@@ -17,39 +15,7 @@
 
 
 <body>
-    
-        <header id="page-header">
-        <div class="page-header__logo logo"><a href="/index.php?r=site/index"></a></div>
-        <div class="page-header__description">
-            Круглосуточный сервисный центр №1
-        </div>
-        <div class="page-header__time">
-            <div class="clock-icon"></div>
-            <div class="time"></div>
-            <div class="flag"></div>
-            <div class="count">1</div>
-            <div class="arrows">
-                <div class="arrow_up"></div>
-                <div class="arrow_down"></div>
-            </div>
-        </div>
-        <div class="page-header__user-name">
-            <div class="user-name"><?php 
-                if (!Yii::app()->user->isGuest) {
-                    $empl = new Employees; $empl->getModelPk(Yii::app()->user->Employee_id); echo($empl['EmployeeName'] );
-                } else {
-                    echo 'Гость';
-                }
-            ?></div>
-            <div class="user-position"><?php if (!Yii::app()->user->isGuest) {$empl = new Employees; $empl->getModelPk(Yii::app()->user->Employee_id); echo($empl['PositionName'] );} ?></div>
-        </div>
-<!--        <div class="page-header__logout">
-            <a href="/index.php?r=site/logout"></a>
-        </div>-->
-        <!--<div style="display: float"></div>-->
-    </header>
-    
-            <div id='page-content'>
+        <div id='page-content'>
                 <?php
                     if(isset($this->title) && isset($this->breadcrumbs)) {
                 ?>
@@ -59,50 +25,8 @@
                     </div>
                 </div>
                 
-                
-                
-                
                 <?php } echo $content; ?>
 
             </div>
-
-    <script type="text/javascript">
-        $(document).ready(function () {
-            
-            (function(){
-                var date = new Date();
-                var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
-                var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-                var time = hours + ':' + minutes;
-                $('.time').html(time);
-                window.setTimeout(arguments.callee, 5000);
-            })();
-            
-            $('.main-menu >ul>li>ul>li').hover(
-                function() {
-                    $(this).parent().prev().addClass('active-li');
-                },
-                function() {
-                    $(this).parent().prev().removeClass('active-li');
-                }
-            );
-            $('.main-menu >ul>li>ul>li>ul>li').hover(
-                function() {
-                    $(this).parent().parent().addClass('active-li');
-                },
-                function() {
-                    $(this).parent().parent().removeClass('active-li');
-                }
-            );
-    
-            $(".menu-btn").on("click", function () {
-                $(".main-menu").toggle(100, function(){$(window).resize();});
-            });
-
-            $(".filter-btn__wrapper").on("click", function () {
-                $(".page-content__filters").toggle(100, function(){$(window).resize();});
-            });
-        });
-    </script>
 </body>
 </html>
--- protected/views/test/test2.php
+++ protected/views/test/test2.php
@@ -2,12 +2,19 @@
 <script>
     $(document).ready(function(){
         
+        
+       
         $("#chbTest1").jqxCheckBox($.extend(true, {}, CheckBoxDefaultSettings, { width: 160, height: 25 }));
 //        $("#rbTest2").jqxRadioButton({ width: 250, height: 25, checked: true});
         $("#edTest3").jqxInput($.extend(true, {}, CheckBoxDefaultSettings, {width: '150px'}));
+        $('#jqxTabs').jqxTabs({ width: '90%', height: 200, position: 'top'});
     });
 </script>
 
+<div style="display: flex">
+    <div>1</div>
+    <div>2</div>
+</div>
 
 <div style="margin-left: 50%; margin-top: 300px">
     <div id="chbTest1">Тест 1</div>
@@ -15,10 +22,22 @@
 
     <!--<div id="rbTest2">Тест 2</div>-->
     <!--<div id="rbTest2" role="radio" tabindex="0" class="jqx-widget jqx-radiobutton" aria-checked="true" aria-disabled="false" style="width: 250px; height: 25px; cursor: pointer; line-height: 25px;"><div class="jqx-fill-state-normal jqx-radiobutton-default" style="width: 13px; height: 13px; margin-top: 5px;"><div style="width: 13px; height: 13px;"><span style="width: 6.5px; height: 6.5px; opacity: 1;" class="jqx-fill-state-pressed jqx-radiobutton-check-checked"></span></div></div>Тест 2<div style="clear: both;"></div><input type="hidden" value="true"></div>-->
-    <!--<input type="text" id="edTest3" />-->
-    <input type="text" id="edTest3" role="textbox" aria-autocomplete="both" aria-disabled="false" aria-readonly="false" aria-multiline="false" class="jqx-widget-content jqx-widget-content-fresh2 jqx-input jqx-input-fresh2 jqx-widget jqx-widget-fresh2 jqx-rc-all jqx-rc-all-fresh2" placeholder="" style="width: 150px; height: 25px;">
+    <input type="text" id="edTest3" />
+    <!--<input type="text" id="edTest3" role="textbox" aria-autocomplete="both" aria-disabled="false" aria-readonly="false" aria-multiline="false" class="jqx-widget-content jqx-widget-content-fresh2 jqx-input jqx-input-fresh2 jqx-widget jqx-widget-fresh2 jqx-rc-all jqx-rc-all-fresh2" placeholder="" style="width: 150px; height: 25px;">-->
 </div>
 
+<div id='jqxTabs'>
+    <ul>
+        <li style="margin-left: 30px;">Тест 1</li>
+        <li>Тест 2</li>
+    </ul>
+    <div>
+        1
+    </div>
+    <div>
+        2
+    </div>
+</div>
 
 
 123123
